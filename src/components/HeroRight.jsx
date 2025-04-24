45e34f9bb1dc15a3782e40874c8f77953a1398c1
Hector Ramirez <67031386+hectorlil48@users.noreply.github.com>
2025-04-23 20:23:22 -0500

    Put all the inputs inside divs to add an error message under it.

--- src/components/HeroRight.jsx
+++ src/components/HeroRight.jsx
@@ -41,39 +41,67 @@ const HeroRight = () => {
         onSubmit={handleSubmit}
         className="flex flex-col gap-4 rounded-[10px] bg-white p-6 shadow-[0_8px_0_0_rgba(0,0,0,0.14)] lg:p-10"
       >
-        <input
-          type="text"
-          name="firstName"
-          onChange={(e) => setFirstName(e.target.value)}
-          value={firstName}
-          placeholder="First Name"
-          className={`${inputBaseStyles} ${errors.firstName ? "focus:red-300 border-2 border-red-500 focus:border-red-500" : "border-border-grey focus:border-cta-purple border"}`}
-        />
-        <input
-          type="text"
-          name="lastName"
-          onChange={(e) => setLastName(e.target.value)}
-          value={lastName}
-          placeholder="Last name"
-          className={`${inputBaseStyles} ${errors.lastName ? "focus:red-300 border-2 border-red-500 focus:border-red-500" : "border-border-grey focus:border-cta-purple border"}`}
-        />
-        <input
-          type="email"
-          name="email"
-          onChange={(e) => setEmail(e.target.value)}
-          value={email}
-          autoComplete="email"
-          placeholder="Email Address"
-          className={`${inputBaseStyles} ${errors.email ? "focus:red-300 border-2 border-red-500 focus:border-red-500" : "border-border-grey focus:border-cta-purple border"}`}
-        />
-        <input
-          type="password"
-          name="password"
-          onChange={(e) => setPassword(e.target.value)}
-          value={password}
-          placeholder="Password"
-          className={`${inputBaseStyles} ${errors.password ? "focus:red-300 border-2 border-red-500 focus:border-red-500" : "border-border-grey focus:border-cta-purple border"}`}
-        />
+        <div>
+          <input
+            type="text"
+            name="firstName"
+            onChange={(e) => setFirstName(e.target.value)}
+            value={firstName}
+            placeholder="First Name"
+            className={`${inputBaseStyles} ${errors.firstName ? "focus:red-300 border-2 border-red-500 focus:border-red-500" : "border-border-grey focus:border-cta-purple border"}`}
+          />
+          {errors.firstName && (
+            <p className="text-firered mt-[6px] text-right text-[11px] font-medium italic">
+              First Name cannot be empty
+            </p>
+          )}
+        </div>
+        <div>
+          <input
+            type="text"
+            name="lastName"
+            onChange={(e) => setLastName(e.target.value)}
+            value={lastName}
+            placeholder="Last name"
+            className={`${inputBaseStyles} ${errors.lastName ? "focus:red-300 border-2 border-red-500 focus:border-red-500" : "border-border-grey focus:border-cta-purple border"}`}
+          />
+          {errors.lastName && (
+            <p className="text-firered mt-[6px] text-right text-[11px] font-medium italic">
+              Last Name cannot be empty
+            </p>
+          )}
+        </div>
+        <div>
+          <input
+            type="email"
+            name="email"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            autoComplete="email"
+            placeholder="Email Address"
+            className={`${inputBaseStyles} ${errors.email ? "focus:red-300 border-2 border-red-500 focus:border-red-500" : "border-border-grey focus:border-cta-purple border"}`}
+          />
+          {errors.email && (
+            <p className="text-firered mt-[6px] text-right text-[11px] font-medium italic">
+              Email cannot be empty
+            </p>
+          )}
+        </div>
+        <div>
+          <input
+            type="password"
+            name="password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            placeholder="Password"
+            className={`${inputBaseStyles} ${errors.password ? "focus:red-300 border-2 border-red-500 focus:border-red-500" : "border-border-grey focus:border-cta-purple border"}`}
+          />
+          {errors.password && (
+            <p className="text-firered mt-[6px] text-right text-[11px] font-medium italic">
+              Password cannot be empty
+            </p>
+          )}
+        </div>
         <button className="bg-btn-green hover:bg-btn-hover rounded-[5px] py-[15px] text-[15px] leading-[26px] font-semibold tracking-[1px] uppercase shadow-[inset_0_-4px_0_0_rgba(0,0,0,0.0909)]">
           Claim Your Free Trail
         </button>
